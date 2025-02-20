79481be17a9f1243bdee1747d8e7f2a7f698d256
KaveeshaSewmini <kaveesha3sewmini2003@gmail.com>
2025-02-20 22:32:02 +0530

    Add files via upload
    Added corrected file uploaded

--- first.c
+++ first.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    char name[20];
-    int a,b;
-    float c,f;
-    char val[] = "Welcome"; a= 10;
-    float x = 3.73
-
-    printf("Enter your name: ");scan("%s", &name);printf(val);
-    printf("\nYour name is %d.",name);c=90.0*3.0
-    printf("\nValue of c: %f",c);x= 49;
-    printf("\nValue of X: %f",x);
-    printf("Hello\tWorld\n\n");
-    printf("You\nare\nlearning\n\c\'language\n\"Do you know C language\"");
-
+    int id = 101;
+    char section = "B";
+    float marks = 89.5;
+    printf("ID:%d,Section:%c,Marks:%.2f\n",id,section,marks);
     return 0;
 }
